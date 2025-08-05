07fa1315724c8ffe452b5f38281e36c810d75508
JayaharishJ192462001 <192462001.simats@saveetha.com>
2025-08-05 13:41:17 +0530

    Add files via upload

---
+++ sort sentence.py
@@ -0,0 +1,10 @@
+# Input from user
+s=input("Enter a sentence: ")
+
+# Split into words, sort, and join back
+words=s.split()
+words.sort()
+
+# Print sorted words
+print("Sorted sentence:")
+print(" ".join(words))
